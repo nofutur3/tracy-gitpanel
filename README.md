da3899190357281cc42e39a1ba37e9e48c2988bf
Jakub Vyvazil <jakub@n3d.cz>
2016-07-17 10:41:22 +0200

    initial commit

--- README.md
+++ README.md
@@ -4,21 +4,47 @@
 [![Downloads this Month](https://img.shields.io/packagist/dm/nofutur3/tracy-gitpanel.svg)](https://packagist.org/packages/nofutur3/tracy-gitpanel)
 [![Latest stable](https://img.shields.io/packagist/v/nofutur3/tracy-gitpanel.svg)](https://packagist.org/packages/nofutur3/tracy-gitpanel)
 
-git panel extension for nette framework
+This is simple extension for [Tracy](https://tracy.nette.org/en/) which shows the information about current GIT branch
+ (Have you ever worked in wrong branch? Nope? So you are probably don't need this one.)
+and slightly more useful things. Just check it out.
 
-## installation
+## Installation
 
-install library via composer
+The recommended installation is using [composer](https://getcomposer.org/). 
+
+_If you are not still using composer, you should check it out. It's 2016(+) afterall._
 
 ```
 composer require nofutur3/tracy-gitpanel
 ```
+Alternative way - in case you are not able to use composer. Download the source code (ie clone git repo) into your project
+and require it some way. For [nette framework](https://nette.org/en/) like this in your bootstrap file:
+```
+$configurator
+    ->createRobotLoader()
+    ->addDirectory(__DIR__ . 'path/to/library/');
+```
 
-register panel
+## Configuration
 
+In your nette application it's simple, just add these lines to your config file. Depends on the structure of your config files,
+but you may use the basic config.neon file. I usually add the lines just in config.local.neon file because I don't need this extension
+in production mode.
+
+##### Nette 2.3+
+```
+tracy:
+    bar:
+        - Nofutur3\GitPanel\Diagnostics\Panel
+```
+
+##### Older version of Nette:
 ```
 nette:
     debugger:
         bar: 
             - Nofutur3\GitPanel\Diagnostics\Panel
 ```
+
+
+
--- src/Nofutur3/GitPanel/Diagnostics/Panel.php
+++ src/Nofutur3/GitPanel/Diagnostics/Panel.php
@@ -22,9 +22,12 @@ class Panel implements IBarPanel
      */
     function getTab()
     {
-
+        $style = '';
+        if ($this->getBranchName() == "master" || $this->getBranchName() == 'staging') {
+            $style = 'background:#dd4742;color:white;padding:3px 4px 4px';
+        }
         $icon = '<svg viewBox="10 10 512 512"><path fill="#f03c2e" d="M 502.34111,278.80364 278.79809,502.34216 c -12.86794,12.87712 -33.74784,12.87712 -46.63305,0 l -46.4152,-46.42448 58.88028,-58.88364 c 13.68647,4.62092 29.3794,1.51948 40.28378,-9.38732 10.97012,-10.9748 14.04307,-26.80288 9.30465,-40.537 l 56.75401,-56.74844 c 13.73383,4.73404 29.56829,1.67384 40.53842,-9.31156 15.32297,-15.3188 15.32297,-40.15196 0,-55.48356 -15.3341,-15.3322 -40.16175,-15.3322 -55.50254,0 -11.52454,11.53592 -14.37572,28.47172 -8.53182,42.6722 l -52.93386,52.93048 0,-139.28512 c 3.73267,-1.84996 7.25863,-4.31392 10.37114,-7.41756 15.32295,-15.3216 15.32295,-40.15196 0,-55.49696 -15.32296,-15.3166 -40.16844,-15.3166 -55.48025,0 -15.32296,15.345 -15.32296,40.17536 0,55.49696 3.78727,3.78288 8.17299,6.64472 12.85234,8.5604 l 0,140.57336 c -4.67935,1.91568 -9.05448,4.75356 -12.85234,8.56264 -11.60533,11.60168 -14.39801,28.6378 -8.4449,42.89232 L 162.93981,433.11336 9.6557406,279.83948 c -12.8743209,-12.88768 -12.8743209,-33.768 0,-46.64456 L 233.20978,9.65592 c 12.87017,-12.87456 33.74338,-12.87456 46.63305,0 l 222.49828,222.50316 c 12.87852,12.87876 12.87852,33.76968 0,46.64456"/></svg>';
-        $label = '<span class="tracy-label">' . $this->getBranchName() . '</span>';
+        $label = '<span class="tracy-label" style="'.$style.'">' . $this->getBranchName() . '</span>';
         return $icon . $label;
     }
 
@@ -34,34 +37,41 @@ class Panel implements IBarPanel
      */
     function getPanel()
     {
-        $title = '<h1>GIT</h1>';
-        $cntTable = '';
+        if ($this->isUnderVersionControl()) {
+            $title = '<h1>GIT</h1>';
+            $warning = '';
+            $cntTable = '';
 
-        // commit message
-        if ($this->getLastCommitMessage() != null) {
-            $cntTable .= '<tr><td>Last commit</td><td> ' . $this->getLastCommitMessage() . ' </td></tr>';
+            if ($this->getBranchName() == 'master' || $this->getBranchName() == 'staging') {
+                $warning = '<p style="color: #dd4742; font-weight: 700;">You are working in ' . $this->getBranchName() . ' branch</p>';
+            }
+
+            // commit message
+            if ($this->getLastCommitMessage() != null) {
+                $cntTable .= '<tr><td>Last commit</td><td> ' . $this->getLastCommitMessage() . ' </td></tr>';
+            }
+
+            // heads
+            if ($this->getHeads() != null) {
+                $cntTable .= '<tr><td>Branches</td><td> ' . $this->getHeads() . ' </td></tr>';
+            }
+
+            // remotes
+            if ($this->getRemotes() != null) {
+                $cntTable .= '<tr><td>Remotes</td><td> ' . $this->getRemotes() . ' </td></tr>';
+            }
+
+            // tags
+            if ($this->getTags() != null) {
+                $cntTable .= '<tr><td>Tags</td><td> ' . $this->getTags() . ' </td></tr>';
+            }
+
+            $content = '<div class=\"tracy-inner tracy-InfoPanel\"><table><tbody>' .
+                $cntTable .
+                '</tbody></table></div>';
+
+            return $title . $warning . $content;
         }
-
-        // heads
-        if ($this->getHeads() != null) {
-            $cntTable .= '<tr><td>Branches</td><td> ' . $this->getHeads() . ' </td></tr>';
-        }
-
-        // remotes
-        if ($this->getRemotes() != null) {
-            $cntTable .= '<tr><td>Remotes</td><td> ' . $this->getRemotes() . ' </td></tr>';
-        }
-
-        // tags
-        if ($this->getTags() != null) {
-            $cntTable .= '<tr><td>Tags</td><td> ' . $this->getTags() . ' </td></tr>';
-        }
-
-        $content = '<div class=\"tracy-inner tracy-InfoPanel\"><table><tbody>' .
-            $cntTable .
-            '</tbody></table></div>';
-
-        return $title . $content;
     }
 
     protected function getBranchName()
@@ -122,7 +132,12 @@ class Panel implements IBarPanel
     {
         $dir = $this->getDirectory();
 
-        $files = scandir($dir . '/.git/refs/remotes');
+        try {
+            $files = scandir($dir . '/.git/refs/remotes');
+        } catch (\ErrorException $e) {
+            return null;
+        }
+
         $message = '';
 
         if ($dir && is_array($files)) {
@@ -176,5 +191,16 @@ class Panel implements IBarPanel
         }
 
         return $dir;
+    }
+
+    private function isUnderVersionControl()
+    {
+        $dir = $this->getDirectory();
+        $head = $dir . '/.git/HEAD';
+
+        if ($dir && is_readable($head)) {
+            return true;
+        }
+        return false;
     }
 }
